a33854020020c1617ec0e6e35d537bfa55ce8cb6
Edward Felix <eddalm04@gmail.com>
2021-08-31 18:09:36 -0400

    [ITA-2014] Add some code

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 console.log('hi there')
 
-function add(x, y) {
-  return x + y
+function add(x, y, z) {
+  return x + y + z
 }
+
+class SomeEntity {
+  name;
+  id;
+  whatever;
+}
+
+const x = new SomeEntity();
+x.id = 1;
+x.name = 3;
